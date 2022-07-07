1e5611ee6c9786536f31bc5514323c122a8556ac
Thales Macêdo <thalesmacedo1@live.com>
2022-07-07 11:52:03 -0300

    added formsmodel to ngmodel declaration

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,7 +9,7 @@ import { Hero } from '../hero';
 export class HeroesComponent implements OnInit {
   hero: Hero = {
     id: 1,
-    name: 'Windstorm'
+    name: 'Windstorm' 
   };
 
   constructor() { }
